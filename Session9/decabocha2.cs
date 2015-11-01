2057c16fea04953b3a625f92a744b61c31acfdd7
yaju <hirapon@mtj.biglobe.ne.jp>
2015-11-01 16:05:32 +0900

    Session9の資料追加

---
+++ Session9/decabocha2.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class decabocha2 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void FixedUpdate()
+    {
+        Rigidbody2D rigidbody = GetComponent<Rigidbody2D>();
+        float h = Input.GetAxisRaw("Horizontal");
+
+        switch ((int)h)
+        {
+            case 0:
+                //GetComponent<Animator>().SetTrigger("Idle");
+                break;
+            case -1:
+                GetComponent<Animator>().SetTrigger("Left");
+                break;
+            case 1:
+                GetComponent<Animator>().SetTrigger("Right");
+                break;
+        }
+    }
+}
