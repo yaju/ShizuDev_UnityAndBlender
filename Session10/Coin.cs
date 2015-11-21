7025c773081d1a0fca5c55c5e2d769fce8ed886a
yaju <hirapon@mtj.biglobe.ne.jp>
2015-11-21 14:08:16 +0900

    セッション10の資料追加

---
+++ Session10/Coin.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Coin : MonoBehaviour {
+
+    public int speed = 2;
+    private static int count = 0;
+
+    void Start ()
+    {
+        GetComponent<Rigidbody2D>().velocity = transform.right.normalized * speed;
+    }
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.tag == "DamageObject")
+        {
+            FindObjectOfType<Enemy>().EnemyDamage();
+            if (++count >= 6)
+            {
+                FindObjectOfType<Enemy>().EnemyDie();
+                Destroy(other.gameObject, 0.3f);
+            }
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ Session10/Enemy.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour {
+
+    public GameObject waypoints;
+    public float speed = 0.1f;
+
+    private int cur = 0;
+    private AudioSource sound01;
+    private AudioSource sound02;
+
+    void Awake()
+    {
+        AudioSource[] audioSources = GetComponents<AudioSource>();
+        sound01 = audioSources[0];
+        sound02 = audioSources[1];
+    }
+    
+    void FixedUpdate()
+    {
+        Transform child = waypoints.transform.GetChild(cur);
+        if (transform.position != child.position)
+        {
+            Vector2 p = Vector2.MoveTowards(transform.position,
+                                            child.position,
+                                            speed);
+            GetComponent<Rigidbody2D>().MovePosition(p);
+        }
+        else cur = (cur + 1) % waypoints.transform.childCount;
+    }
+
+    public void EnemyDamage()
+    {
+        sound01.PlayOneShot(sound01.clip); ///敵ダメージ
+
+    }
+
+    public void EnemyDie()
+    {
+        sound02.PlayOneShot(sound02.clip); //敵死亡
+    }
+}
